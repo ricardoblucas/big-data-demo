99aa0aa22d7ad7cc7631ba79a456f6ed5f4441e4
Ricardo <ricardo.lucas@edp.pt>
2023-01-30 01:53:23 +0000

    added configurations for k8s

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vn1bntp554zjl-2)iej54%ko=yqjyxxbpa)-yeu22!(94t0#-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
