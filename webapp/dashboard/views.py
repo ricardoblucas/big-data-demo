ed6e20d06c23e7899dfadd8f59f964d55b77f419
Ricardo <ricardo.lucas@edp.pt>
2023-01-30 04:13:03 +0000

    updated django view

--- webapp/dashboard/views.py
+++ webapp/dashboard/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Here you will see a beautifull dashboard with machine learned wind predictions and a reporting chat for Guincho windsurfers here. Arriving in two days.")
+    return HttpResponse("Hello, world. Here you will  see a beautifull dashboard with machine learned wind predictions and a reporting chat for Guincho windsurfers here. Arriving in two days.")
